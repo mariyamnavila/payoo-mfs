22a9b6d3b0cf62acc6617473781844d3b4b370ca
mariyamnavila <bibimariyamnavila@gmail.com>
2025-07-11 21:34:30 +0600

    added add money option

--- js/home.js
+++ js/home.js
@@ -7,8 +7,31 @@ document.getElementById('btn-add-money')
         event.preventDefault();
         // step 2 get money to be added d to the account
         const addMoneyInput = document.getElementById('input-add-money').value;
-        console.log(addMoneyInput)
+        console.log(addMoneyInput);
         // get pin number provided
         const pinNumberInput = document.getElementById('input-pin-number').value;
-        console.log(pinNumberInput)
+        console.log(pinNumberInput);
+
+        // step-3 verify the pin number
+        if (pinNumberInput === '1234') {
+            console.log('adding money to your account');
+            // step-4 get the current balance
+
+            const balance = document.getElementById('account-balance').innerText;
+            console.log(balance);
+            // step-5 add addMoneyInput with balance
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + balanceNumber;
+            console.log(newBalance);
+
+            // step-6 update the balance in UI/DOM
+            document.getElementById('account-balance').innerText = newBalance;
+
+        } else {
+            alert('Failed to add money! Please try again.')
+        }
     })
+
+
+    
